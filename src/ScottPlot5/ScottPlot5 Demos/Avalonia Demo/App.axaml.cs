472fc867254ecfa73b08a397645a90ace5ba507a
Олег Трушков <oleg_trushkov@hotmail.com>
2026-04-08 15:51:46 +0300

    bump Avalonia to 12.0.0 version

--- src/ScottPlot5/ScottPlot5 Demos/Avalonia Demo/App.axaml.cs
+++ src/ScottPlot5/ScottPlot5 Demos/Avalonia Demo/App.axaml.cs
@@ -1,11 +1,8 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core;
-using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using Avalonia_Demo.ViewModels;
 using Avalonia_Demo.Views;
-using System.Linq;
 
 namespace Avalonia_Demo
 {
@@ -20,9 +17,6 @@ namespace Avalonia_Demo
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
-                // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-                // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-                DisableAvaloniaDataAnnotationValidation();
                 desktop.MainWindow = new MainWindow
                 {
                     DataContext = new MainWindowViewModel(),
@@ -30,19 +24,6 @@ namespace Avalonia_Demo
             }
 
             base.OnFrameworkInitializationCompleted();
-        }
-
-        private void DisableAvaloniaDataAnnotationValidation()
-        {
-            // Get an array of plugins to remove
-            var dataValidationPluginsToRemove =
-                BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
-
-            // remove each entry found
-            foreach (var plugin in dataValidationPluginsToRemove)
-            {
-                BindingPlugins.DataValidators.Remove(plugin);
-            }
         }
     }
 }
